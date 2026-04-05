3ecbf64fbdb88550af644c3b1e6b13c74ee16013
AbirSaafi02 <saafiabir15@gmail.com>
2026-04-05 16:11:42 +0100

    init: setup projet GuardNet backend J1/2

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from database import engine
+from models import models
+from routers import scan
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="GuardNet API", version="1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(scan.router)
+
+@app.get("/")
+def root():
+    return {"message": "GuardNet API operationnelle"}
